44500742ba5bdb90c3372ecb9f730d0accab8a86
ishaan khan <122585129+Ishaankhan7@users.noreply.github.com>
2024-12-01 13:48:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,13 @@ const path = require('path')
 
 const dirname = path.resolve();
 
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.mongoURL).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((error)=>{
+    console.log("Error while connecting to MongoDB",error)
+})
 
 app.use(
   cors({
